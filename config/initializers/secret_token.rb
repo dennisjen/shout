969f396a420a720f920aa5a55993a19c74ec5afe
Dennis Jen <djen@Dennis-Jens-MacBook-Pro.local>
2011-03-03 13:20:05 -0500

    initial commit for intro to ruby on rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shout::Application.config.secret_token = '269e88315565212eeb1f1ec40b5f6aa04810a90450704005e58f4113fe398599fe267c2e83ad9f0558430247f4ab349142c4a08a92170cc2d87507334c168a18'
